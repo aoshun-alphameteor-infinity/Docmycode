8397563093e6ea25c5fa86eb10e465d207e39d54
karineS <karine.siette@laposte.net>
2013-01-16 11:27:51 -0800

    Create Site.c

---
+++ src/Site.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE* init_site(){
+FILE* f=fopen("index.html","w+");
+fprintf(f,"<html>\n <body>\n ");
+
+
+fprintf(f,"</body>\n </html>\n");
+return f;
+}
+
+FILE* create_page_file(char* page_name){
+FILE* f=fopen(page_name,"w+");
+
+
+return f;
+}
+
+FILE* create_page_function(char* page_name){
+FILE* f=fopen(page_name,"w+");
+
+
+return f;
+}
+

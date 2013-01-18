17ed55f51269e6f6763d3199d019c05c5c145db3
aoshun alphameteor infinity <aoshun.alphameteor@hotmail.fr>
2013-01-18 18:03:14 +0100

    fin de doc

--- header/StringList.h
+++ header/StringList.h
@@ -20,13 +20,42 @@
         
         typedef struct _StringList_ StringList;
         
-        
+        /**
+        \fn StringList* newStringList (void)
+        \brief allocate a structure StringList
+        \return a pointer on the new StringList
+        */
         StringList* newStringList (void);
+        /**
+        \fn void freeStringList (StringList* strlist)
+        \param strlist  the StringList need to be free
+        \brief free a StringList
+        */
         void freeStringList (StringList* strlist);
+        /**
+        \fn void reinitStringList (StringList* strlist)
+        \brief return the Stringlist at empty StringListlist state
+        */
         void reinitStringList (Stringlist* strlist);
+        /**
+        \fn int addtoStringList (StringList* strlist, char* String)
+        \brief add String to the StringList strlist
+        \return EXIT_SUCCESS if the add was successful or EXIT_FAILURE in other case
+        */
         int addtoStringList (StringList* strlist, char* String);
         
+        /**
+        \fn StringCell* newStringCell (char* String)
+        \brief allocate a structure StringCell
+        \return a pointer on the new StringCell
+        \param the String which will be stored in the StringCell structure
+        */
         StringCell* newStringCell (char* String);
+        /**
+        \fn void freeStringCell (StringCell* strlist)
+        \param cell  the StringCell need to be free
+        \brief free a StringCell
+        */
         void freeStringCell (StringCell* cell);
         
 #endif
